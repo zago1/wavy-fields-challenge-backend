433af03663ecfda09be53b1cf4df200d4c3efc1a
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-24 18:36:05 -0300

    add create user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Request, Response, Router } from "express";
+import { createUserController } from "./useCases/CreateUser";
 
 const router = Router();
 
 router.get('/', (request: Request, response: Response) => {
   return response.status(200).send("OK");
+});
+
+router.post('/user', (request: Request, response: Response) => {
+  return createUserController.handle(request, response);
 });
 
 
