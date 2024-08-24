ccc2c0b9392ca8fa04e8ca5d86085d7c59c313af
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-24 18:45:43 -0300

    add delete user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { createUserController } from "./useCases/CreateUser";
+import { deleteUserController } from "./useCases/DeleteUser";
 
 const router = Router();
 
@@ -9,6 +10,10 @@ router.get('/', (request: Request, response: Response) => {
 
 router.post('/user', (request: Request, response: Response) => {
   return createUserController.handle(request, response);
+});
+
+router.delete('/user/:id', (request: Request, response: Response) => {
+  return deleteUserController.handle(request, response);
 });
 
 
