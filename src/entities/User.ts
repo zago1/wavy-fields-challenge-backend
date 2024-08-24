0c35b714c7db8ac26aa88db8e8fe5d53f5fadd0e
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-24 18:55:32 -0300

    fix User props

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -3,7 +3,7 @@ export class User {
 
   public name: string;
   public email: string;
-  public password: string;
+  public password: string | undefined;
 
   constructor(props: Omit<User, 'id'>, id?: string) {
     Object.assign(this, props);
