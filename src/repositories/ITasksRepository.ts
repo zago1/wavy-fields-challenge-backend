313397be02263d345314bcf98e90dc7bebccc39b
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-24 18:16:18 -0300

    add delete task function; add pagination on listByUser function

--- src/repositories/ITasksRepository.ts
+++ src/repositories/ITasksRepository.ts
@@ -1,5 +1,4 @@
 import { Task } from "../entities/Task";
-
 interface IPaginatedResponse<T> {
   metadata: { totalRegisters: number };
   data: T
@@ -7,7 +6,8 @@ interface IPaginatedResponse<T> {
 
 export interface ITasksRepository {
   create(text: string, userId: string): Promise<Task>;
-  listByUser(userId: string): Promise<IPaginatedResponse<Task[]>>;
+  listByUser(userId: string, skip: number, take: number): Promise<IPaginatedResponse<Task[]>>;
   updateText(text: string, id: string): Promise<Task>;
   updateDone(done: boolean, id: string): Promise<Task>;
+  delete(id: string): Promise<void>;
 }
