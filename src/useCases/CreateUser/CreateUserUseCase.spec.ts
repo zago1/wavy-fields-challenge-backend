23b9c71b4850d0e14850b9fa97a7d5d84c98e1a4
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-25 14:39:43 -0300

    add CreateUserUseCase tests

---
+++ src/useCases/CreateUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,32 @@
+import { mock, Mock } from 'ts-jest-mocker';
+import { CreateUserUseCase } from './CreateUserUseCase';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+import { User } from '../../entities/User';
+
+describe('CreateUserUseCase', () => {
+  const user: User = new User({
+    name: 'User Test',
+    email: 'user.test@gmail.com',
+    password: 'password'
+  }, 'user-id');
+
+  let userRepository: Mock<IUsersRepository>;
+  let createUserUseCase: CreateUserUseCase;
+
+  beforeEach(() => {
+    userRepository = mock<IUsersRepository>();
+    createUserUseCase = new CreateUserUseCase(userRepository);
+  });
+
+  it('should return return User', async () => {
+    userRepository.create.mockResolvedValueOnce(
+      new Promise((resolve) => resolve(user))
+    );
+
+    const response = await createUserUseCase.execute(user.name, user.email, user.password ?? '');
+
+    expect(userRepository.create).toHaveBeenCalledTimes(1);
+    expect(response).toBeInstanceOf(User);
+    expect(response).toEqual(user);
+  });
+});
